4994993f30bb95c3edbd4ece29b54280e4fdd0f5
silent-tempest <brussian768@gmail.com>
2018-11-02 23:32:34 +0700

    Update get-style.js (make private)

--- internal/get-style.js
+++ internal/get-style.js
@@ -1,13 +1,14 @@
 'use strict';
 
 /**
- * @method getStyle
+ * @private
+ * @method _getStyle
  * @param  {object}  element
  * @param  {string}  style
  * @param  {object} [computedStyle]
  * @return {string}
  */
-module.exports = function getStyle ( element, style, computedStyle ) {
+module.exports = function _getStyle ( element, style, computedStyle ) {
   return element.style[ style ] ||
     ( computedStyle || getComputedStyle( element ) ).getPropertyValue( style );
 };
