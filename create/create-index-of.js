0e03f5f80e4897b50afdc2395b0ec28a892821dd
silent-tempest <brussian768@gmail.com>
2018-11-03 00:33:21 +0700

    Update createIndexOf

--- create/create-index-of.js
+++ create/create-index-of.js
@@ -1,10 +1,17 @@
 'use strict';
 
 var baseIndexOf = require( '../base/base-index-of' );
+
 var toObject    = require( '../to-object' );
 
+/**
+ * @private
+ * @method createIndexOf
+ * @param  {boolean}  fromRight
+ * @return {function}
+ */
 module.exports = function createIndexOf ( fromRight ) {
-  return function indexOf ( arr, search, fromIndex ) {
-    return baseIndexOf( toObject( arr ), search, fromIndex, fromRight );
+  return function indexOf ( array, value, fromIndex ) {
+    return baseIndexOf( toObject( array ), value, fromIndex, fromRight );
   };
 };
