050b7564194774cd27ec31cd18f156373c3bc12e
silent-tempest <brussian768@gmail.com>
2018-12-21 04:55:01 +0700

    Delete ES5 Object.keys polyfill.

--- keys.js
+++ keys.js
@@ -1,31 +1,12 @@
 'use strict';
 
-var baseKeys = require( './base/base-keys' );
-var toObject = require( './to-object' );
 var support  = require( './support/support-keys' );
 
+var toObject = require( './to-object' );
+
 if ( support !== 'es2015' ) {
-  var _keys;
-
-  /**
-   * + ---------------------------------------------------------------------- +
-   * | I tested the functions with string[2048] (an array of strings) and had |
-   * | this results in Node.js (v8.10.0):                                     |
-   * + ---------------------------------------------------------------------- +
-   * | baseKeys x 10,674 ops/sec ±0.23% (94 runs sampled)                     |
-   * | Object.keys x 22,147 ops/sec ±0.23% (95 runs sampled)                  |
-   * | Fastest is "Object.keys"                                               |
-   * + ---------------------------------------------------------------------- +
-   */
-
-  if ( support === 'es5' ) {
-    _keys = Object.keys;
-  } else {
-    _keys = baseKeys;
-  }
-
-  module.exports = function keys ( v ) {
-    return _keys( toObject( v ) );
+  module.exports = function keys ( value ) {
+    return Object.keys( toObject( value ) );
   };
 } else {
   module.exports = Object.keys;
--- support/support-keys.js
+++ support/support-keys.js
@@ -1,17 +1,7 @@
 'use strict';
 
-var support;
-
-if ( Object.keys ) {
-  try {
-    support = Object.keys( '' ), 'es2015'; // eslint-disable-line no-unused-expressions, no-sequences
-  } catch ( error ) {
-    support = 'es5';
-  }
-} else if ( { toString: null }.propertyIsEnumerable( 'toString' ) ) {
-  support = 'has-a-bug';
-} else {
-  support = 'not-supported';
+try {
+  module.exports = Object.keys( '' ), 'es2015'; // eslint-disable-line no-unused-expressions, no-sequences
+} catch ( error ) {
+  module.exports = 'es5';
 }
-
-module.exports = support;
