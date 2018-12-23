0dbbf8255e62125b3ee448b687ffd185ad588525
silent-tempest <brussian768@gmail.com>
2018-12-23 07:49:20 +0700

    Create callIteratee.

---
+++ internal/call-iteratee.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function callIteratee ( fn, ctx, val, key, obj ) {
+  if ( typeof ctx === 'undefined' ) {
+    return fn( val, key, obj );
+  }
+
+  return fn.call( ctx, val, key, obj );
+};
